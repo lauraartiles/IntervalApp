6c643d192156cc022576ff4aa3adaced4a07f439
Laura A <laura.artiles@mlb.com>
2017-03-23 18:15:36 -0400

    Starting to work on create workout view

--- IntervalTimer/CreateWorkoutViewController.swift
+++ IntervalTimer/CreateWorkoutViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+  let cellIdentifier = "CellIdentifier"
+  let tempLabels = ["Warm-up", "Interval length", "Number of intervals", "Cool down", "Total time"]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
 
+  func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 1
+  }
 
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return tempLabels.count
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+
+    // Fetch Fruit
+    let label = tempLabels[indexPath.row]
+
+    // Configure Cell
+    cell.textLabel?.text = label
+
+    return cell
+  }
 }
 
--- IntervalTimer/WorkoutViewController.swift
+++ IntervalTimer/WorkoutViewController.swift
@@ -10,61 +10,63 @@ import UIKit
 import Foundation
 
 class WorkoutViewController: UIViewController {
-    var timer = Timer()
-    var elapsedTime = 0
-    var timerDidStart = false
-    @IBOutlet weak var timeLabel: UILabel!
-    @IBOutlet weak var startAndResetButton: UIButton!
-    
-    @IBOutlet weak var stopButton: UIButton!
-    @IBAction func startTimer(_ sender: UIButton) {
-        if timerDidStart {
-            timer.invalidate()
-            timeLabel.text = "00:00"
-            elapsedTime = 0
-            timerDidStart = false
-            startAndResetButton.setTitle("Start", for: .normal)
-        }
-        else {
-            startAndResetButton.setTitle("Reset", for: .normal)
-            timer = Timer.scheduledTimer(timeInterval: 1, target:self, selector: #selector(WorkoutViewController.updateTimeLabel), userInfo: nil, repeats: true)
-            timerDidStart = true
-        }
-        
-        
-    }
-    @IBAction func stopTimer(_ sender: UIButton) {
-        timer.invalidate()
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        startAndResetButton.layer.cornerRadius = 5
-        stopButton.layer.cornerRadius = 5
+  var timer = Timer()
+  var elapsedTime = 0
+  var timerDidStart = false
+  @IBOutlet weak var timeLabel: UILabel!
+  @IBOutlet weak var startAndResetButton: UIButton!
 
-        // Do any additional setup after loading the view.
+  @IBOutlet weak var stopButton: UIButton!
+  @IBAction func startTimer(_ sender: UIButton) {
+    if timerDidStart {
+      timer.invalidate()
+      timeLabel.text = "00:00"
+      elapsedTime = 0
+      timerDidStart = false
+      startAndResetButton.setTitle("Start", for: .normal)
+    }
+    else {
+      startAndResetButton.setTitle("Reset", for: .normal)
+      timer = Timer.scheduledTimer(timeInterval: 1, target:self, selector: #selector(WorkoutViewController.updateTimeLabel), userInfo: nil, repeats: true)
+      timerDidStart = true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    func updateTimeLabel() {
-        let minutes = Int(elapsedTime / 60)
-        let seconds =  elapsedTime - minutes * 60
-        timeLabel.text = String(format:"%02i:%02i",minutes,Int(seconds))
-        elapsedTime += 1
-    }
-    
 
-    /*
-    // MARK: - Navigation
+  }
+  @IBAction func stopTimer(_ sender: UIButton) {
+    timer.invalidate()
+  }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    startAndResetButton.layer.cornerRadius = 5
+    stopButton.layer.cornerRadius = 5
+    timeLabel.layer.masksToBounds = true
+    timeLabel.layer.cornerRadius = 10
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    // Do any additional setup after loading the view.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  func updateTimeLabel() {
+    let minutes = Int(elapsedTime / 60)
+    let seconds =  elapsedTime - minutes * 60
+    timeLabel.text = String(format:"%02i:%02i",minutes,Int(seconds))
+    elapsedTime += 1
+  }
+
+
+  /*
+   // MARK: - Navigation
+
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destinationViewController.
+   // Pass the selected object to the new view controller.
+   }
+   */
 
 }
